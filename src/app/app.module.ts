5c26c3f7e72d2b5664efcecbec2ba358847687a5
ukgk.silva <ukgk.silva@gmail.com>
2022-01-29 19:48:27 +0530

    Home and create event pages updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { LayoutModule } from 'src/app/layout/layout.module';
 import {MatStepperModule} from '@angular/material/stepper';
 import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
+import {NgxMatDatetimePickerModule, NgxMatNativeDateModule, NgxMatTimepickerModule} from '@angular-material-components/datetime-picker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { HomeComponent } from './features/home/home.component';
 import { RegistrationComponent } from './features/registration/registration.component';
 import { LoginComponent } from './features/auth/login/login.component';
@@ -33,12 +36,19 @@ import { CreateEventComponent } from './features/event/create-event/create-event
     BrowserAnimationsModule,
     SharedModule,
     LayoutModule,
+    FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatStepperModule,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    MatDatepickerModule,
+    NgxMatDatetimePickerModule,
+    NgxMatTimepickerModule,
+    NgxMatNativeDateModule,
+    // MatMomentDateModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
